c369da240e1c02d36705eb1bef597ced4de3108e
sushmak49 <sushmak49@gmail.com>
2020-11-22 13:20:14 +0530

    Added HomePage restaurantCard

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,17 +1,141 @@
-//Sample HomePage 
+// //Sample HomePage 
 
-import React, {Component} from 'react';
-//import './Home.css';
+// import React, {Component} from 'react';
+// //import './Home.css';
+// import Header from '../../common/header/Header';
+
+// class Home extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <Header showSearchBox={true} baseUrl={this.props.baseUrl}/>
+//             </div>
+//         )    
+//     }
+// }
+
+// export default Home;
+
+import React, { Component } from 'react';
 import Header from '../../common/header/Header';
+//import { Route, Link } from 'react-router-dom';
+import * as Utils from "../../common/Utils/Utils";
+// import * as Constants from "../../common/Constants";
+import RestaurantCard from '../card/RestaurantCard.js';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from "@material-ui/core/styles";
+import './Home.css';
 
-class Home extends Component{
-    render(){
-        return(
+
+const styles = {
+    resCard: { width: "90%", cursor: "pointer" }
+};
+
+class Home extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            imageData: [],
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        this.getAllImageData();
+    }
+
+    // Get all restuarants data
+    getAllImageData = () => {
+        const requestUrl = this.props.baseUrl + "restaurant";
+        const that = this;
+        Utils.makeApiCall(
+            requestUrl,
+            null,
+            null,
+            'GET',
+            null,
+            responseText => {
+                that.setState(
+                    {
+                        imageData: JSON.parse(responseText).restaurants
+                    }
+                );
+            },
+            () => { }
+        );
+    };
+
+    //Logout action from drop down menu on profile icon
+    loginredirect = () => {
+        sessionStorage.clear();
+        this.props.history.push({
+            pathname: "/"
+        });
+        window.location.reload();
+    }
+
+    // Restaurant search by name
+    searchRestaurantsByName = event => {
+        console.log('inside event')
+        let loginData = null;
+        const searchValue = event.target.value;
+        const requestUrl = this.props.baseUrl + "restaurant/name/" + searchValue;
+        const that = this;
+        let xhrLogin = new XMLHttpRequest();
+        xhrLogin.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    that.setState(
+                        {
+                            imageData: JSON.parse(this.responseText).restaurants
+                        }
+                    );
+                }
+            }
+        });
+        let url = requestUrl;
+        xhrLogin.open("GET", url);
+        //xhrLogin.setRequestHeader("authorization", "Basic " + window.btoa(this.state.loginContactNo + ":" + this.state.loginPassword));
+        xhrLogin.setRequestHeader("Content-Type", "application/json");
+        xhrLogin.setRequestHeader("Cache-Control", "no-cache");
+        xhrLogin.setRequestHeader("Access-Control-Allow-Origin", "*");
+        xhrLogin.send(loginData);
+    };
+
+    render() {
+        const { classes } = this.props;
+        return (
             <div>
-                <Header showSearchBox={true} baseUrl={this.props.baseUrl}/>
-            </div> 
-        )    
+                <Header logoutHandler={this.loginredirect} baseUrl={this.props.baseUrl} searchRestaurantsByName={this.searchRestaurantsByName} showSearchBox={true} history={this.props.history} />
+                <div className="mainContainer">
+                    {
+                        this.state.imageData === null ? <span style={{ fontSize: "20px" }}>No restaurant with the given name</span>
+                            : (
+                                (this.state.imageData || []).map((resItem, index) =>
+                                    <div key={"div" + index} className="restaurantCard">
+                                        <Grid className="gridCard" key={index}>
+                                            <RestaurantCard
+                                                resId={resItem.id}
+                                                resURL={resItem.photo_URL}
+                                                resName={resItem.restaurant_name}
+                                                resFoodCategories={resItem.categories}
+                                                resCustRating={resItem.customer_rating}
+                                                resNumberCustRated={resItem.number_customers_rated}
+                                                avgPrice={resItem.average_price}
+                                                classes={classes}
+                                                index={index}
+                                            />
+                                        </Grid>
+                                    </div>
+                                )
+                            )
+                    }
+                </div>
+
+            </div>
+        )
     }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
