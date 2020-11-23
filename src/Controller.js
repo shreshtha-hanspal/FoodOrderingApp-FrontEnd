e494e093a6cc957bb05ba40bb9f57cee70f3f926
sushmak49 <sushmak49@gmail.com>
2020-11-23 13:34:44 +0530

    Added route information for details and checkout page

--- src/Controller.js
+++ src/Controller.js
@@ -5,7 +5,9 @@ import { Route, Switch } from "react-router-dom";
 
 //Imports of different pages in the application
 import Home from './screens/home/Home';
+import Details from './screens/details/Details.js';
 import Profile from '../src/common/profile/Profile.js';
+import Checkout from '../src/screens/checkout/Checkout.js';
 
 class Controller extends Component {
     constructor() {
@@ -16,10 +18,21 @@ class Controller extends Component {
         return (
             <Switch>
                 <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl}/>} />
+                <Route exact path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl}/>} />
                 <Route exact path="/profile" render={() => <Profile />}></Route>
+                {/* Routing to the checkout page */}
+                {/* <Route path='/checkout' render={(props) => (
+                        sessionStorage.getItem('customer-cart') === null ? (
+                            Redirect to='/'
+                        ) : (
+                            <Checkout {...props} baseUrl={this.baseUrl} />
+                        )
+                    )} /> */}
+                <Route exact path="/checkout" render={props=> <Checkout {...props} baseUrl={this.baseUrl}/>} />
             </Switch>
         )
     }
 }
 
 export default Controller;
+
